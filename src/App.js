0e5ba248b0deacf9e6c831ea702eea6042609d58
Saviour Gidi <profsaviour@gmail.com>
2020-10-03 10:08:09 +0000

    [add] ci and dependencies installation

--- src/App.js
+++ src/App.js
@@ -20,9 +20,21 @@ function App() {
 
         </a>
       </header>
-      <div>
-        lorem ipsum
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+
+        </a>
+      </header>
     </div>
   );
 }
